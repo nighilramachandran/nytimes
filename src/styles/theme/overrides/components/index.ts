8f5c3df66d6c55c3e06bbcc464e9c02dabf14208
nighilramachandran <nighilramm@gmail.com>
2024-07-17 16:56:26 +0400

    remove unused imports

--- src/styles/theme/overrides/components/index.ts
+++ src/styles/theme/overrides/components/index.ts
@@ -1,9 +1,3 @@
-import { buttonOverride } from "./Button";
-import { inputOverride } from "./Input";
-import { paperOverride } from "./Paper";
-
 export const componetnsOverrides = {
-  ...paperOverride,
-  ...inputOverride,
-  ...buttonOverride,
+  // ...paperOverride,
 };
